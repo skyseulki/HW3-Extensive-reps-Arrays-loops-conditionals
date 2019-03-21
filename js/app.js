edf498ef4a281ecd528f970e4cd0836d241207ce
Sky Seulki <admin@Skys-MacBook-Pro.local>
2019-03-20 17:18:17 -0700

    commit 3 - excited kittens answered

--- js/app.js
+++ js/app.js
@@ -9,3 +9,20 @@ for (let i = 0; i <= 200; i++) {
         console.log(i);   
     }
 }
+
+//Excited Kitten
+// #1
+for (let i = 1; i <= 20; i++) {
+    console.log("Love me, pet me! HSSSSS!");
+}
+// #2
+const phrases = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+ 
+
+for (let i = 0; i < 20; i++) {
+    console.log("Love me, pet me! HSSSSS!");
+    if ( i % 2 === 0 ) {
+        let random = Math.floor(Math.random()*3);
+        console.log(phrases[random]);
+    }
+}
