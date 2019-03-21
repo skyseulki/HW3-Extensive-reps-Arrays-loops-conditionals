07d50cef4eed80ed3baf0ec26eb6e77071bef854
Sky Seulki <admin@Skys-MacBook-Pro.local>
2019-03-21 00:34:57 -0700

    commit 9 - project euler problem 1 answered

--- js/app.js
+++ js/app.js
@@ -180,4 +180,11 @@ for (let i = 0; i < kristynsCloset.length; i++){
 //     },
 // ];
 
-
+//Multiples of 3 and 5
+let sum = 0;
+for (let i = 0; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0){
+        sum += i;
+    }
+}
+console.log(sum);
