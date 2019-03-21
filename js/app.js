b268eedf1559029450ce62df062e8cf746ca0796
Sky Seulki <admin@Skys-MacBook-Pro.local>
2019-03-20 17:36:07 -0700

    commit 4 - fizz buzz answered

--- js/app.js
+++ js/app.js
@@ -26,3 +26,22 @@ for (let i = 0; i < 20; i++) {
         console.log(phrases[random]);
     }
 }
+
+//Fizz Buzz
+// #1
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+
+// #2
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if ( i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
